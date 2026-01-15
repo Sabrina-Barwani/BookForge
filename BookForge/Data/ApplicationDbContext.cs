7ffe5d6796d24056e37e46565fc2ea79f5e78db6
Sabrina-Barwani <sabrinalbrwani@gmail.com>
2026-01-15 14:40:51 +0400

    Add Category table

---
+++ BookForge/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookForge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookForge.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // create table Categories in database
+        public DbSet <Category> Categories { get; set; }
+    }
+}
